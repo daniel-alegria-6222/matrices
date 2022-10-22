bf4cb5000994f85ab5aa354fb0f167ee10f2dd5e
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2022-10-22 15:08:47 -0500

    moved module sumRow from Matriz.lua to utils.lua

--- utils.lua
+++ utils.lua
@@ -13,10 +13,19 @@ function utils.transformTable( arr1, arr2, func, length  )
     -- 'arr1' is transformed with the elements of 'arr2' through 'func'
     -- ejemplo de func: function func(a, b) a + 2b end
     -- length parameter is optional
-    length = length or #arr1
+    local length = length or #arr1
     for i=1, length do
         arr1[i] = func( arr1[i], arr2[i] )
     end
+end
+
+function utils.sumTable( table, length )
+    local length = lenght or #table
+    local sum = 0
+    for i=1, length do
+        sum = sum + table[i]
+    end
+    return sum
 end
 
 return utils
