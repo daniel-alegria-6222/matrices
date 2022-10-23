db13524639db4c0c2ea060e7a20708321031ed8f
Daniel Alegria Sallo <ccusdanielalegriasallo@gmail.com>
2022-10-22 23:58:41 -0500

    updated the code a shit ton

--- matriz.lua
+++ matriz.lua
@@ -27,39 +27,11 @@ function Matriz.mt.__tostring(m)
         end
         str = str .. "|\n"
     end
-    return str .. ""
+    return "\n" .. str .. ""
 end
 
 -- FUNCIONES UTILES
-function Matriz.sumarRow( m, i )
-    if i < 1 or i > m.i then return nil end
-    local sum = 0
-    for j=1, m.j do
-        sum = sum + m.arr[i][j]
-    end
-    return sum
-end
-
-function Matriz.sumarCol( m, j )
-    if j < 1 or j > m.j then return nil end
-    local sum = 0
-    for i=1, m.i do
-        sum = sum + m.arr[i][j]
-    end
-    return sum
-end
-
-function Matriz.sumarTodo( m )
-    local sum = 0
-    for i=1, m.i do
-        for j=1, m.j do
-            sum = sum + m.arr[i][j]
-        end
-    end
-    return sum
-end
-
-function Matriz.esDivisibleRow( m, i, num )
+function Matriz.esRowDivisible( m, i, num )
     -- checkea si una fila 'i' es divisible por un numero 'num'
     if i < 1 or i > m.i or num == 0 then return nil end
     for j=1, m.j do
@@ -68,30 +40,44 @@ function Matriz.esDivisibleRow( m, i, num )
     return true
 end
 
-function Matriz.esDivisibleCol( m, j, num )
-    -- checkea si una columna 'j' es divisible por un numero 'num'
-    if j < 1 or j > m.j or num == 0 then return nil end
-    for i=1, m.i do
-        if m.arr[i][j] % num ~= 0 then return false end
-    end
-    return true
-end
-
 function Matriz.swapRow( m, r1, r2 )
     if r1 == r2 then return end
-    local _r1 = m.arr[r1]
-    m.arr[r1] = r2
-    m.arr[r1] = _r1
+    local arr_r1 = m.arr[r1]
+    m.arr[r1] = m.arr[r2]
+    m.arr[r2] = arr_r1
 end
 
-function Matriz.swapCol( m, c1, c2 )
-    if r1 == r2 then return end
-    local temp
-    for i=1, m.i do
-        temp = m.arr[i][c1]
-        m.arr[i][c1] = m.arr[i][c2]
-        m.arr[i][c2] = temp
+function Matriz.concatRight( m1, m2 )
+    local new_t = m1.arr
+    if m1.i ~= m2.i then return nil end
+    for i=1, m2.i do
+        for j=1, m2.j do
+            elem = m2.arr[i][j]
+            new_t[i][j+m1.j] = elem
+        end
     end
+    return Matriz.new(new_t)
+end
+
+function Matriz.contarCeroRows( m )
+    -- contar las filas con solo ceros
+    local count = 0
+    for i=1, m.i do
+        if utl.countTable( m.arr[i] ) == 0 then count = count + 1 end
+    end
+    return count
+end
+
+function Matriz.spliceByColumns( m , j1, j2 )
+    -- te da una parte de la matriz delimitada por las columnas j1 y j2
+    local new_t = {}
+    for i=1, m.i do
+        new_t[i] = {}
+        for j=j1, j2 do
+            new_t[i][j+1-j1] = m.arr[i][j]
+        end
+    end
+    return Matriz.new(new_t)
 end
 
 -- OPERACIONES ENTRE MATRICES
@@ -186,7 +172,6 @@ function Matriz.mt.__mul (m1, m2) -- parameters have to be 'number' or 'Matriz'
     end
 
     return Matriz.new(new_t)
-
 end
 
 function Matriz.mt.__pow (m, n)
@@ -263,49 +248,100 @@ function Matriz.getConjugada( m )
     -- TODO: No urgente. Um, creo que solo yo podria implemetar esta funcion, requiere modulo 'imaginario'
 end
 
-function Matriz.getEscalonada( m )
+function Matriz.getEscalonada( m , ... )
+    -- Los argumentos opcionales son otras matrices a las cuales aplicar las 
+    -- operaciones se uso para que 'm' se escalonara.
+
+    matrices = {...}
     -- TODO: arreglar este desastre completamente
-    local new_m = m
-    local elem, guia_n
+    -- local new_m = m
+    -- local elem
 
-    local j = 1
-    while j <= m.j do
-        if guia == nil then
-            for i=1, m.i do
-                elem = m.arr[i][j]
-                if elem == 1 then
-                    guia_n = i
-            end
-        else j = j + 1 end
-    end
+    -- local j = 1
+    -- while j <= m.j do
+    --         for i=j, m.i do
+    --             elem = m.arr[i][j]
+    --             if elem == 1 then
+    --                 break
+    --         end
+    -- end
 
-    return new_m
+    -- return new_m
 end
 
 function Matriz.getRango( m )
-    -- TODO: implementar esta funcion usando el modulo getEscalonada y la teoria
+    local escalonada = m:getEscalonada()
+    if escalonada then
+        return escalonada.i - escalonada:contarCeroRows()
+    end
+end
+
+function Matriz.getIdentidadDeEscalonada( m , ... )
+    -- transforma la matriz escalonada 'm' con una serie de operaciones, y tambien
+    -- las aplica a los parametros opcionales
+
+    -- TODO: implementar esta funcion
+    matrices = {...}
 end
 
 function Matriz.getInversaPorGauss( m )
     if m.i ~= m.j then return nil end
     -- TODO: implementar esta funcion usando el modulo getEscalonada y la teoria
+
+    -- local escalonada = m:concatRight(Matriz.INDETIDAD(m.j)):getEscalonada()
+    -- if escalonada then
+    --     local result = escalonada:spliceByColumns(1, m.j)
+    --     local inversa = escalonada:spliceByColumns(m.j+1, joined.j)
+    -- end
 end
 
-function Matriz.getDeterminante( m )
-    if m.i ~= m.j then return nil end
-    -- TODO: implementar esta funcion
+function Matriz.getDeterminante( m , length )
+    if m.i ~= m.j or m.i < 2 then return nil end
+    local length = length or m.i
+
+    if length == 2 then
+        return m.arr[1][1] * m.arr[2][2] - m.arr[2][1] * m.arr[1][2]
+
+    elseif length == 3 then
+        local suma1 = 0
+        local suma2 = 0
+        local mul1, mul2
+
+        for n=0, length-1 do
+            mul1 = 1; mul2 = 1
+
+            for j=0, length-1 do
+                _i = (j+n) % length
+                mul1 = mul1 * m.arr[_i+1][j+1]
+                mul2 = mul2 * m.arr[_i+1][length-j]
+            end
+            suma1 = suma1 + mul1
+            suma2 = suma2 + mul2
+        end
+
+        return suma1 - suma2
+    else
+        return m:getDeterminantePorCofactores()
+    end
 end
 
 function Matriz.getInversaPorAdjunta( m )
     if m.i ~= m.j then return nil end
-    -- TODO: implementar esta funcion usando el modulo getDeterminante y la teoria
+    return ( 1 / m:getDeterminante() ) * m:getAdjunta()
 end
 
 Matriz.getInversa = Matriz.getInversaPorAdjunta
 
-function Matriz.getMenorComplementario( m, i, j )
-    if m.i ~= m.j then return nil end
-    -- TODO: implementar esta funcion usando la teoria
+function Matriz.getMenorComplementario( m, i0, j0 )
+    if m.i ~= m.j or m.i < 2 then return nil end
+    local new_t = {}
+    for i=1, m.i-1 do
+        new_t[i] = {}
+        for j=1, m.j-1 do
+            new_t[i][j] = m.arr[ i<i0 and i or i+1 ][ j<j0 and j or j+1 ]
+        end
+    end
+    return Matriz.new(new_t):getDeterminante()
 end
 
 function Matriz.getCofactor( m, i, j )
@@ -314,16 +350,21 @@ function Matriz.getCofactor( m, i, j )
 end
 
 function Matriz.getDeterminantePorCofactores( m )
-    -- TODO: No urgente, implementar esta funcion usando la teoria
+    if m.i ~= m.j then return nil end
+    local suma = 0
+    for j=1, m.j do
+        suma = suma + m.arr[1][j] * m:getCofactor(1,j)
+    end
+    return suma
 end
 
 function Matriz.getMatrizDeCofactores( m )
     if m.i ~= m.j then return nil end
 
     local new_t = {}
-    for i=1, #m.i do
+    for i=1, m.i do
         new_t[i] = {}
-        for j=1, #m.j do
+        for j=1, m.j do
             new_t[i][j] = m:getCofactor(i,j)
         end
     end
